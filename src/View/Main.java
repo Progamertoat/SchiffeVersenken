7f039cf73691ae0e7c09f057e8bf8d61505bbb58
Progamertoat <leon12steam@hotmail.com>
2026-05-21 11:36:47 +0200

    lol

--- src/View/Main.java
+++ src/View/Main.java
@@ -2,15 +2,17 @@ package View;
 
 import Business.Boards.Board;
 import Business.Boards.FriendlyBoard;
-import View.Rendering.SwingRenderer;
-import View.Rendering.TerminalRenderer;
+
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
-    public static void main() throws IOException, InterruptedException {
-        new SwingRenderer().init(new FriendlyBoard(10), "Yo", 1000, 1000, 0, 0);
+    public static void main() {
 
     }
 }
